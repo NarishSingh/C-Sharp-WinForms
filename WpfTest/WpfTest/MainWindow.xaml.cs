e39a31e74b39bd52d0ed742e0ac6cac51a419060
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-15 14:47:34 -0400

    Test project made from .NET docs tutorial

---
+++ WpfTest/WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Button Click event handler - add a name to log
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !listNames.Items.Contains(txtName))
+            {
+                listNames.Items.Add(txtName.Text);
+                
+                txtName.Clear();
+            }
+        }
+    }
+}
