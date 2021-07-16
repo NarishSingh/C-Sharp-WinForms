093b57fcd7fa9e25d93f8d9279973c44d4aec739
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-16 15:56:52 -0400

    Refactoring, attempt to implement key press handler

--- CalculatorWpf/CalculatorWpf/MainWindow.xaml.cs
+++ CalculatorWpf/CalculatorWpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.Windows;
+using System.Windows.Input;
 
 namespace CalculatorWpf
 {
@@ -20,140 +21,105 @@ namespace CalculatorWpf
         /*NUM PAD EVENT HANDLERS*/
         private void Btn7_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 7; //shift over one place, then add the number clicked
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 7;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(7);
         }
 
         private void Btn8_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 8;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 8;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(8);
         }
 
         private void Btn9_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 9;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 9;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(9);
         }
 
         private void Btn4_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 4;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 4;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(4);
         }
 
         private void Btn5_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 5;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 5;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(5);
         }
 
         private void Btn6_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 6;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 6;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(6);
         }
 
         private void Btn1_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 1;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 1;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(1);
         }
 
         private void Btn2_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 2;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 2;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(2);
         }
 
         private void Btn3_OnClick(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(_operation))
-            {
-                _num1 = _num1 * 10 + 3;
-                TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                _num2 = _num2 * 10 + 3;
-                TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
-            }
+            ValidateNum(3);
         }
 
         private void Btn0_OnClick(object sender, RoutedEventArgs e)
         {
+            ValidateNum(0);
+        }
+
+        private void NumPadKeyPress(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.NumPad0:
+                    ValidateNum(0);
+                    break;
+                case Key.NumPad1:
+                    ValidateNum(1);
+                    break;
+                case Key.NumPad2:
+                    ValidateNum(2);
+                    break;
+                case Key.NumPad3:
+                    ValidateNum(3);
+                    break;
+                case Key.NumPad4:
+                    ValidateNum(4);
+                    break;
+                case Key.NumPad5:
+                    ValidateNum(5);
+                    break;
+                case Key.NumPad6:
+                    ValidateNum(6);
+                    break;
+                case Key.NumPad7:
+                    ValidateNum(7);
+                    break;
+                case Key.NumPad8:
+                    ValidateNum(8);
+                    break;
+                case Key.NumPad9:
+                    ValidateNum(9);
+                    break;
+            }
+        }
+        
+        /// <summary>
+        /// Validate btn press and change display based on input
+        /// </summary>
+        /// <param name="num">digit to validate</param>
+        private void ValidateNum(int num)
+        {
             if (string.IsNullOrEmpty(_operation))
             {
-                _num1 *= 10;
+                _num1 = _num1 * 10 + num; //shift over one place, then add the number clicked
                 TextDisplay.Text = _num1.ToString(CultureInfo.InvariantCulture);
             }
             else
             {
-                _num2 *= 10;
+                _num2 = _num2 * 10 + num;
                 TextDisplay.Text = _num2.ToString(CultureInfo.InvariantCulture);
             }
         }
