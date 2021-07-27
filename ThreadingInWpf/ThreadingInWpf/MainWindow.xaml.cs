f71be27cb34293c220437963f06dc5687403fc80
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-27 14:15:35 -0400

    marshalling delegate to ui thread

--- ThreadingInWpf/ThreadingInWpf/MainWindow.xaml.cs
+++ ThreadingInWpf/ThreadingInWpf/MainWindow.xaml.cs
@@ -8,18 +8,31 @@ namespace ThreadingInWpf
     /// </summary>
     public partial class MainWindow
     {
+        private SynchronizationContext _uiSyncContext;
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+            _uiSyncContext = SynchronizationContext.Current; //capture current UI thread
+
             new Thread(Work).Start(); //worker thread
         }
 
         void Work()
         {
             Thread.Sleep(5000);
-            Action action = () => TxtDisplay.Text = "-The Game-";
+            UpdateMsg("-The Game-");
+        }
 
-            Dispatcher.BeginInvoke(action); //enqueues the action to the message queue which handles UI threading
+        private void UpdateMsg(string msg)
+        {
+            //using dispatcher
+            // Action action = () => TxtDisplay.Text = msg;
+            // Dispatcher.BeginInvoke(action); //enqueues the action to the message queue which handles UI threading
+            
+            //marshalling delegate to UI thread
+            _uiSyncContext.Post( _ => TxtDisplay.Text = msg, null);
         }
     }
 }
