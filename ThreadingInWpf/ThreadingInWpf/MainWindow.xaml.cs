de47043429a10b7ff7f971b89804e3f8088c24e7
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-07-27 13:59:35 -0400

    Threading in WPF

---
+++ ThreadingInWpf/ThreadingInWpf/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingInWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            new Thread(Work).Start(); //worker thread
+        }
+
+        void Work()
+        {
+            Thread.Sleep(5000);
+            Action action = () => TxtDisplay.Text = "-The Game-";
+
+            Dispatcher.BeginInvoke(action); //enqueues the action to the message queue which handles UI threading
+        }
+    }
+}
